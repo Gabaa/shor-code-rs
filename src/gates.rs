da70ecef53f955f18cccae49e43091becaa2ca59
Gabaa <Gabaa@users.noreply.github.com>
2022-11-21 20:29:39 +0100

    Checkpoint before implementing(?) permutation matrices

--- src/gates.rs
+++ src/gates.rs
@@ -17,29 +17,43 @@ pub fn z(state: &mut State<2>) {
     todo!()
 }
 
-pub fn cnot(state: &mut State<4>) {
-    let u = Matrix4::<Complex64>::from_iterator(
-        [
-            One::one(),
-            Zero::zero(),
-            Zero::zero(),
-            Zero::zero(),
-            Zero::zero(),
-            One::one(),
-            Zero::zero(),
-            Zero::zero(),
-            Zero::zero(),
-            Zero::zero(),
-            Zero::zero(),
-            One::one(),
-            Zero::zero(),
-            Zero::zero(),
-            One::one(),
-            Zero::zero(),
-        ]
-        .into_iter(),
-    );
+pub fn cnot<const N: usize>(state: &mut State<N>, control: usize, target: usize) {
+    let u = Matrix4::<Complex64>::from_vec(vec![
+        One::one(),
+        Zero::zero(),
+        Zero::zero(),
+        Zero::zero(),
+        Zero::zero(),
+        One::one(),
+        Zero::zero(),
+        Zero::zero(),
+        Zero::zero(),
+        Zero::zero(),
+        Zero::zero(),
+        One::one(),
+        Zero::zero(),
+        Zero::zero(),
+        One::one(),
+        Zero::zero(),
+    ]);
 
+    // TODO: figure out how to create a larger CNOT matrix that only affects the relevant
+    // coefficients in the state.
+    // You can do tensor product of matrices (e.g. I otimes X does nothing to first qubit and flips
+    // second). Will this require us to reorder some of the entries in the state vector?
+    // 16/11: YES! Just need to create "permutation matrix" that reorders the entries in the state
+    // vector, and then apply that before and apply the inverse after.
+    // (a_1, a_2, a_3, ...)^T
+    // a_1 |00000> + a_2 |00001> + a_3 + |00010> + ...
+    // a_i |00000> + a_j |
+    //
+    // Swap b1 and b2.
+    // a_1 |00> + a_2 |01> + a_3 |10> + a_4 |11>
+    // a_1 |00> + a_3 |01> + a_2 |10> + a_4 |11>
+    //
+    // Swap b1 and b3.
+    // a_1 |000> + a_2 |001> + a_3 |010> + a_4 |011> + a_5 |100> + a_6 |101> + a_7 |110> + a_8 |111>
+    // a_1 |000> + a_2 |001> + a_3 |010> + a_4 |011> + a_5 |100> + a_6 |101> + a_7 |110> + a_8 |111>
     state.coefficients = u * state.coefficients;
 }
 
